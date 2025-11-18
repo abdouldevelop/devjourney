5e292c35b74f01ea7e4a5087a72a741d1a7c8ae6
abdouldevelop <abdouldevelop@gmail.com>
2025-11-18 21:39:44 +0000

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+// Année dynamique dans le footer
+document.getElementById('camara').textContent = "Bienvenue sur SHE IS THE CODE!!!"
+document.getElementById('cam').textContent = "Bienvenue sur SHE IS THE CODE!!!"
